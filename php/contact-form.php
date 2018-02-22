558053a45969de377609bbab9324086837dc4838
jaimindeveloper <jaimin.suthar12@gmail.com>
2018-02-22 10:20:41 +0530

    Newmaili

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,7 +1,6 @@
 <?php
-header("Access-Control-Allow-Orgin: *");
-header("Access-Control-Allow-Methods: *");
-
+header('Access-Control-Allow-Origin: https://jaimindeveloper.github.io/');
+header('Access-Control-Allow-Methods: POST');
 $name = $_POST['name'];
 $email = $_POST['email'];
 $mobileno = $_POST['mobile'];
